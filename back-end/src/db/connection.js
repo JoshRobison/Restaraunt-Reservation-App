fb63f03aa22f0309dd3cb4fbe194b249c813a4f1
Joshua Robison <118320419+JoshRobison@users.noreply.github.com>
2023-05-01 11:29:30 -0700

    Add files via upload

---
+++ back-end/src/db/connection.js
@@ -0,0 +1,10 @@
+const environment = process.env.NODE_ENV || "development";
+const config = require("../../knexfile")[environment];
+const knex = require("knex")(config);
+
+module.exports = knex;
+const environment = process.env.NODE_ENV || "development";
+const config = require("../../knexfile")[environment];
+const knex = require("knex")(config);
+
+module.exports = knex;
